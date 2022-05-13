36392c5a849b0a6c392e4ce7af8f5daa7285311a
Farzad Chinwala <105508002+fchinwal@users.noreply.github.com>
2022-05-13 12:32:16 -0400

    Update streamlit_app.py
    Added a picklist to the menue

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,9 @@ streamlit.text('🧉 Kale, Spinach and Rocket Smoothie')
 streamlit.text('🥚 Hard-Boiled Free-Range Egg')
 streamlit.text('🍞🥑 Avocado Toast')
 streamlit.header('🍌🍓  Build Your Own Fruit Smoothie 🥝🍇')
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+
+# Lets put a picklist here so they can pick the fruits they want in the smoothie
+streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
